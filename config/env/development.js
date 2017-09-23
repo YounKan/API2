4c439a1484c77b0291f2cb160a983f5fedb3c8a4
Sasakorn Kulchittipakk <osasakorn@gmail.com>
2017-09-23 11:24:02 +0700

    test

---
+++ config/env/development.js
@@ -0,0 +1,5 @@
+module.exports = {
+        mongoUri: 'mongodb://admin:pass@localhost:27017/testdb1',
+       debug: true,
+       sessionSecret: 'dev_secret_key'
+}
--- config/express.js
+++ config/express.js
@@ -14,8 +14,14 @@ module.exports = function () {
     }
 
     app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+       extended: true
+}));
+
+
 
     require('../app/routes/index.route')(app);
+    require('../app/routes/user.route')(app);
     
     return app;
 }
